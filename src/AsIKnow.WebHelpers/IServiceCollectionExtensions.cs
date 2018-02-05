b5715e9d484704cc4755cce9e0486b3a530c1f24
goldenass <gianmaria.canossa@gmail.com>
2018-02-05 21:59:50 +0100

    added path base option

--- src/AsIKnow.WebHelpers/IServiceCollectionExtensions.cs
+++ src/AsIKnow.WebHelpers/IServiceCollectionExtensions.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
 
 namespace AsIKnow.WebHelpers
 {
@@ -19,6 +22,20 @@ namespace AsIKnow.WebHelpers
                 .PersistKeysToFileSystem(new DirectoryInfo(options.DataProtection.KeyRingPath));
             if (options.DataProtection.DisableAutomaticKeyGeneration)
                 dpBuilder.DisableAutomaticKeyGeneration();
+        }
+
+        public static IApplicationBuilder UseWebApp(this IApplicationBuilder ext)
+        {
+            ext = ext ?? throw new ArgumentNullException(nameof(ext));
+
+            using (IServiceScope scope = ext.ApplicationServices.CreateScope())
+            {
+                IOptions<WebApplicationOptions> options = scope.ServiceProvider.GetRequiredService<IOptions<WebApplicationOptions>>();
+                if(options.Value.PathBase != null)
+                    ext.UsePathBase(options.Value.PathBase);
+            }
+
+            return ext;
         }
     }
 }
--- src/AsIKnow.WebHelpers/WebApplicationOptions.cs
+++ src/AsIKnow.WebHelpers/WebApplicationOptions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace AsIKnow.WebHelpers
 {
     public class WebApplicationOptions
     {
+        public PathString PathBase { get; set; } = null;
         public DataProtectionOptions DataProtection { get; set; } = new DataProtectionOptions();
     }
 }
