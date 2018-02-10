0adb4e2211e8781b8d8ab322e3eb8c2b99661b78
goldenass <gianmaria.canossa@gmail.com>
2018-02-10 15:22:07 +0100

    pkg update

--- src/AsIKnow.WebHelpers/WebApplicationOptions.cs
+++ src/AsIKnow.WebHelpers/WebApplicationOptions.cs
@@ -7,6 +7,9 @@ namespace AsIKnow.WebHelpers
 {
     public class WebApplicationOptions
     {
+        public Uri IdpApiBase { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
         public PathString PathBase { get; set; } = null;
         public DataProtectionOptions DataProtection { get; set; } = new DataProtectionOptions();
     }
